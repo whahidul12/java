68f6ebbda9472c6da5a6149ce9ae672551904a3b
md-whahid <mdislamwhahid@gmail.com>
2024-05-03 00:02:51 +0600

    first commit

---
+++ project001/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+  public static void main(String[] args){
+    System.out.println("hello");
+    long num = 2222222222222222222L;
+    System.out.print("this is it: " + " //");
+  }
+}
