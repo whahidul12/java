f96318f5f883d9cac8bed7c15180065d49dd069b
md-whahid <mdislamwhahid@gmail.com>
2024-06-20 19:52:28 +0600

    args neo

--- Demo.java
+++ Demo.java
@@ -1,5 +1,25 @@
-class Demo{
-    public static void main(String[] args){}
+class Demo {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 4;
+        Cal neo = new Cal();
+        int mnb = neo.ad(num1, num2);
+        System.out.println(mnb);
+
+        switch (args) {
+            case value:
+
+                break;
+
+            default:
+                break;
+        }
+    }
 }
 
-
+class Cal {
+    public int ad(int a, int b) {
+        int r = a + b;
+        return r;
+    }
+}
