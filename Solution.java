3838438d151bb4ec4de189514f20dbd73f9e5dcc
md-whahid <mdislamwhahid@gmail.com>
2024-06-23 22:35:47 +0600

    inheritance

--- Solution.java
+++ Solution.java
@@ -1,41 +1,28 @@
-class Human {
-  private int age;
-  private String name;
-  private boolean alive;
-  private String aliveState;
-
-  public Human(String a) {
-    System.out.println("this is Human constractor: " + a);
+class A {
+  public A() {
+    System.out.println("in A");
   }
 
-  public void isAlive() {
-    if (alive) {
-      this.aliveState = "alive";
-    } else {
-      this.aliveState = "dead";
-    }
+  public A(int n) {
+    System.out.println("in A int" + n);
+  }
+}
+
+class B extends A {
+  public B() {
+    System.out.println("in B");
   }
 
-  public void setInfo(int age, String name, boolean alive) {
-    this.age = age;
-    this.name = name;
-    this.alive = alive;
-
-  }
-
-  public void getInfo() {
-    isAlive();
-    System.out.println("his name is " + name + ".");
-    System.out.println("he is " + age + " years old.");
-    System.out.println("and he is " + aliveState + ".");
+  public B(int n) {
+    super(7);
+    System.out.println("in B int" + n);
   }
 }
 
 public class Solution {
   public static void main(String[] args) {
-    Human objHuman = new Human("whahid");
-    objHuman.setInfo(12, "Nafi", false);
-    objHuman.getInfo();
-  }
 
+    B objB = new B(5);
+
+  }
 }
