d7fa559eaf6b87c5deb66152e95d643a46400ceb
md-whahid <mdislamwhahid@gmail.com>
2024-06-29 23:30:42 +0600

    problem difec

---
+++ Assignment001/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+  public static void main(String[] args) {
+    int id = 101;
+    String title = "iphone16";
+    int price = 1895;
+    String description = "Apple product";
+
+    System.out.println("ID : " + id);
+    System.out.println("Title : " + title);
+    System.out.println("Price : " + price + "$");
+    System.out.println("Description : " + description);
+
+  }
+
+}
---
+++ assingment003/Product.java
@@ -0,0 +1,15 @@
+package assingment003;
+
+import java.util.Scanner;
+
+public class Product {
+  public static void main(String[] args) {
+    // Scanner scanner = new Scanner(System.in);
+    // int roll = scanner.nextInt();
+    // String name = scanner.nextLine();
+    // // boolean pass = scanner.nextBoolean();
+    // System.out.println(name + " : " + roll + " ");
+    // scanner.close();
+    System.out.println("hello");
+  }
+}
