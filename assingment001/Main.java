0c35fb782a04a8f4611e43844c1f61a8aee7d562
md-whahid <mdislamwhahid@gmail.com>
2024-06-30 20:53:43 +0600

    assingment for java so adding file ///also adjust some changes

--- assingment001/Main.java
+++ assingment001/Main.java
@@ -1,3 +1,4 @@
+package assingment001;
 
 public class Main {
   public static void main(String[] args) {
--- assingment003/Product.java
+++ assingment003/Product.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Product {
   public static void main(String[] args) {
-    // Scanner scanner = new Scanner(System.in);
-    // int roll = scanner.nextInt();
-    // String name = scanner.nextLine();
-    // // boolean pass = scanner.nextBoolean();
-    // System.out.println(name + " : " + roll + " ");
-    // scanner.close();
-    System.out.println("hello");
+    Scanner scanner = new Scanner(System.in);
+    int roll = scanner.nextInt();
+    String name = scanner.nextLine();
+
+    System.out.println(name + " : " + roll + " ");
+    scanner.close();
+
   }
 }
