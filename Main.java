2e965ac8d3f5c4bdc073c5c136c68a2a6da43d83
md-whahid <mdislamwhahid@gmail.com>
2024-07-01 23:31:52 +0600

    Integer class is fire

--- Main.java
+++ Main.java
@@ -2,5 +2,9 @@ public class Main {
 
   public static void main(String[] args) {
 
+    int num = 12;
+    Integer num1 = num;
+    // Integer nuum = Integer.valueOf(num);
+    System.out.println(num1);
   }
 }
