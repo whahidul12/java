423ecbe6f8881375830f09167ae2a10abe09e6c1
md-whahid <mdislamwhahid@gmail.com>
2024-07-04 20:26:47 +0600

    custom generics

---
+++ Int.java
@@ -0,0 +1,13 @@
+public class Int {
+
+  int num;
+
+  public Int() {
+
+  }
+
+  public int fin() {
+    return num;
+  }
+
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    Int tak = new Int();
+    System.out.println(tak);
+
+    Integer tax = 63;
+    System.out.println(tax);
+  }
+}
