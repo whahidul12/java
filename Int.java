31a3a83203e46463d1797b1073d7c430f6495e1c
md-whahid <mdislamwhahid@gmail.com>
2024-07-06 21:05:57 +0600

    int.java

--- Int.java
+++ Int.java
@@ -1,13 +1,23 @@
+class Point {
+
+}
+
+class Bird {
+  Point center;
+  double radious;
+
+  public void setCenter(Integer X, Integer Y) {
+  }
+
+  public void setRadious() {
+
+  }
+}
+
 public class Int {
-
-  int num;
-
-  public Int() {
+  public static void main(String[] args) {
+    Bird obj = new Bird();
+    obj.center = new Point();
 
   }
-
-  public int fin() {
-    return num;
-  }
-
 }
